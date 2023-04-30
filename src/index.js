70080b84c4f2f17047b84c8df9f4d2c8bdde2ad3
Christian Lopez <lopezchristian1610@gmail.com>
2023-04-29 23:49:51 -0400

    challenge 1, get all dogs images completed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('%c HI', 'color: firebrick')
+
+const dogImages = document.getElementById("dog-image-container");
+fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(res => res.json())
+    .then(dogs => {
+        dogs.message.forEach(dog => {
+            const dogImg = document.createElement("img");
+            dogImg.setAttribute("src", dog);
+            dogImages.appendChild(dogImg);
+        })
+        // console.log(dogs);
+    })
